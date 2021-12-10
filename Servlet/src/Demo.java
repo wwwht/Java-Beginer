53695a828a6831933bbf3e0ab9c67faf278e6626
wwwht <wht_no1@outlook.com>
2021-12-10 16:20:22 +0800

    Add Servlet

---
+++ Servlet/src/Demo.java
@@ -0,0 +1,22 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Demo", value = "/Demo")
+public class Demo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //设置网页响应类型
+        response.setContentType("text/html");
+        //实现具体操作    
+        PrintWriter out = response.getWriter();
+        out.println("This is a new servlet page");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ basic/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wht
+ * @Date: 2021-12-08 09:52:23
+ * @LastEditors: wht
+ * @LastEditTime: 2021-12-08 09:53:26
+ */
+// Hello.java
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("I am Hello Class, which is a main class");
+    }
+}
+
+class E {
+    public static void main(String[] args) {
+        System.out.print("I am E Class, which is a main class");
+    }
+}
