79fefe9f85c578206c912678241d71f3c20eacda
wwwht <wht_no1@outlook.com>
2022-02-16 16:06:44 +0800

    add payment

---
+++ test.py
@@ -0,0 +1,10 @@
+'''
+Descripttion: 
+version: 
+Author: wht
+Date: 2022-02-14 11:15:26
+LastEditors: wht
+LastEditTime: 2022-02-14 11:17:49
+'''
+tmp = ['Bob\n', "Joe\n", "Frank\n", "Bill"]
+print([i[0] for i in tmp])
