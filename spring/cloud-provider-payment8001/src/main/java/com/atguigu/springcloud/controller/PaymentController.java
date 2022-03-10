abccee66d263cd8ea9a2049dcedb9158a2092943
wwwht <wht_no1@outlook.com>
2022-03-10 19:28:09 +0800

    add api-commons

--- spring/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ spring/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,14 +1,12 @@
 package com.atguigu.springcloud.controller;
+
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j  //日志
