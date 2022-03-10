be8164b86826770f221689f261d775480dc8b9b3
wwwht <wht_no1@outlook.com>
2022-03-10 10:07:06 +0800

    add consumer

---
+++ spring/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//这三个注解是lombok的，除了导入依赖，idea还需要安装插件（具体操作问度娘）
+@Data   //set/get方法
+@AllArgsConstructor //有参构造器
+@NoArgsConstructor  //无参构造器
+public class CommonResult<T>
+{
+    // 404 not fount
+    private Integer code;
+    private String  message;
+    private T       data;
+
+    public CommonResult(Integer code, String message){
+        this(code, message, null);
+    }
+}
