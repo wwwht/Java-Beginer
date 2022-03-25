0d1971e000d1ead3321f01b5fed5bfa6583d362d
wwwht <wht_no1@outlook.com>
2022-03-25 17:08:07 +0800

    hystrix0325

--- spring/cloud-consumer-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ spring/cloud-consumer-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,8 +24,17 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
+    }
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "消费者80，支付系统繁忙";
+    }
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试！";
     }
 }
--- spring/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ spring/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,8 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -8,19 +11,29 @@ import java.util.concurrent.TimeUnit;
 public class PaymentService {
 
     //正常访问书法
+
     public String paymentInfo_OK(Integer id){
         return "线程池： "+ Thread.currentThread().getName() + "\tpaymentInfo_OK，id：" + id;
     }
 
     //超时访问方法
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            // 设置自身超时调用时间的峰值为3秒，峰值内可以正常运行，超过了需要有兜底的方法处理，服务降级fallback
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
+        int timeNumber = 3000;
         try{
-            TimeUnit.SECONDS.sleep(timeNumber);
+            TimeUnit.MILLISECONDS.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() +
-                "\tpaymentInfo_TimeOut，id：" + id + "，耗时：" + timeNumber + "秒";
+                "\tpaymentInfo_TimeOut，id：" + id + "，耗时：" + timeNumber + " ms";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "8001提供者，线程池：" + Thread.currentThread().getName() +
+                "\tpaymentInfo_TimeOutHandler系统繁忙，请稍后再试，id：" + id;
     }
 }
